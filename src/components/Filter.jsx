388a83c3b77ce2eee2f2f994b72742ea86584e67
Taras Papka <taras.papka@protonmail.com>
2023-05-09 23:03:56 +0300

    added previous project

---
+++ src/components/Filter.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import css from './Phonebook.module.css';
+import PropTypes from 'prop-types';
+
+export class Filter extends Component{
+  render() {
+    return (
+      <>
+      <p className={css.filter_text}>
+        Find contacts by name
+      </p>
+        <input
+        type="text"
+        name="filter"
+        className={css.filter_input}
+        value={this.props.filter}
+        onChange={this.props.filterContact}
+        />
+      </>
+    )
+  }
+}
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  filterContact: PropTypes.func.isRequired,
+}
