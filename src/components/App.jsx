e3502e61245c5dd2316bc678695af0b4651abe6b
Taras Papka <taras.papka@protonmail.com>
2023-05-10 23:48:29 +0300

    corrected code for storing in LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,21 +9,20 @@ import { ContactList } from './ContactList';
 export class App extends Component {
   state = {
     contacts: [
-      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
   };
-    componentDidMount() {
-    const contact = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contact)
-
-      if (parsedContacts) {
-        this.setState({contact: parsedContacts})
-      }
-}
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts")
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
   componentDidUpdate(prevProps,prevState) {
     if (this.state.contacts !== prevState.contacts) {
     localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
@@ -39,6 +38,7 @@ export class App extends Component {
     name,
     number
   };
+
   if (this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
     alert(`${name} is already in contacts`);
   } else {
